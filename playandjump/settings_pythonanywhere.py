e933ba5e13d52801ba330ff8112549541529439a
MitrofanovDm89 <MitrofanovDm89@gmail.com>
2025-10-03 14:28:59 +0300

    Update PythonAnywhere email settings

--- playandjump/settings_pythonanywhere.py
+++ playandjump/settings_pythonanywhere.py
@@ -100,14 +100,15 @@ CACHES = {
     }
 }
 
-# Email settings (настройте под ваш хостинг)
+# Email settings для PythonAnywhere
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@playandjump.de')
+EMAIL_HOST_USER = 'dlktsprdct@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'ЗАМЕНИТЕ_НА_ПАРОЛЬ_ПРИЛОЖЕНИЯ')
+DEFAULT_FROM_EMAIL = 'dlktsprdct@gmail.com'
+EMAIL_CHARSET = 'utf-8'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
